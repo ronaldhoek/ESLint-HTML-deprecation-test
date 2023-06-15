e887f3ec01aebad411a924330d0a79b75246b191
Ronald Hoek <ronald.hoek@componentagro.nl>
2023-06-15 09:04:00 +0200

    Add deprecation rule en deprecated property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ESLint-HTML-deprecation';
+
+  /**
+   * @deprecated Do not use this property
+   */
+  public depecatedProp = '';
+
+  ngOnInit(): void {
+    this.depecatedProp = 'Do not use this'
+  }
 }
