a472621b5a1d2aca61e7eac1cf0d1bb761faa95a
Ronald Hoek <ronald.hoek@componentagro.nl>
2023-11-17 11:38:26 +0100

    Cleanup default code

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ESLint-HTML-deprecation');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ESLint-HTML-deprecation app is running!');
-  });
 });
